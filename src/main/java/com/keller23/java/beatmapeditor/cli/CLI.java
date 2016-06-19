5036a4c1a84d364c73fbc2c37f045a5a68fc9ae4
shaynemk <uskickbutt@gmail.com>
2016-06-19 15:53:47 -0700

    -Fixed up the CLI/CLITest to actually test the output of the help and headers.

--- src/main/java/com/keller23/java/beatmapeditor/cli/CLI.java
+++ src/main/java/com/keller23/java/beatmapeditor/cli/CLI.java
@@ -1,20 +1,27 @@
 package com.keller23.java.beatmapeditor.cli;
 
-import com.keller23.java.beatmapeditor.lib.*;
+import com.keller23.java.beatmapeditor.lib.Strings;
 
+/***
+ * Class for dealing with the CLI
+ */
 public class CLI {
 
+    /***
+     * Returns the string to print to the screen after starting the app.
+     * @return
+     */
     public static String printHeader() {
         return "\n--- " + Strings.APP_NAME  + ", v" + Strings.APP_VERSION + " ---\n"; //+ ", v" + Strings.APP_VERSION;
     }
 
+    /***
+     * Returns the string to print that should be printed to the console.
+     * @return Help Menu
+     */
     public static String printHelp() {
-        // TODO: 5/30/16 Build up the help menu.
-
-        //return "- Help Menu -" + "\nSorry, I'm only a useless WIP placeholder for the time being.\nTry running with filesTest argument.";
-
-        return "--help\t\t\t\t\tThis page, also default.\n" +
-                "--readVersion \"path/to/OSU\"\t\tOutput version of OSU.\n" +
-                "--read \"path/to/OSU\"\t\t\tPrint all properties of OSU file that we are reading in.";
+        return    "--help\t\t\t\t\tThis page, also default.\n"
+                + "--readVersion \"path/to/OSU\"\t\tOutput version of OSU.\n"
+                + "--read \"path/to/OSU\"\t\t\tPrint all properties of OSU file that we are reading in.";
     }
 }
--- src/test/java/com/keller23/java/beatmapeditor/cli/CLITest.java
+++ src/test/java/com/keller23/java/beatmapeditor/cli/CLITest.java
@@ -1,30 +1,57 @@
 package com.keller23.java.beatmapeditor.cli;
 
 import com.keller23.java.beatmapeditor.lib.Strings;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.Assert.assertEquals;
 
 public class CLITest {
+
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+
+    @Before
+    public void setUp() throws Exception {
+        System.setOut(new PrintStream(outContent));
+        System.setErr(new PrintStream(errContent));
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        System.setOut(null);
+        System.setErr(null);
+    }
+
     @Test
     public void printHeader() throws Exception {
-        //Verify Header is properly returned as String.
-
         String expect = "\n--- Beatmap Editor, v" + Strings.APP_VERSION + " ---\n";
 
-        assertSame(expect, CLI.printHeader());
+        System.out.print(CLI.printHeader());
+
+        Assert.assertEquals("Header output to System.out does not match expected.", expect, outContent.toString());
+
+        assertEquals("Actual/Expected strings don't match.", expect, CLI.printHeader());
     }
 
     @Test
     public void printHelp() throws Exception {
         //Verify Help is properly returned as String.
 
-        /*String expect = "\n" +
-                "--filesTest\t\t\tDisplay all files in working & all child directories.\n" +
-                "--searchOSU [dir]\t\tSearch for and display all *.osu files in working dir or [dir].\n";*/
+        String expect =   "--help\t\t\t\t\tThis page, also default.\n"
+                        + "--readVersion \"path/to/OSU\"\t\tOutput version of OSU.\n"
+                        + "--read \"path/to/OSU\"\t\t\tPrint all properties of OSU file that we are reading in.";
 
-        //assertSame(expect, CLI.printHelp());
-        assertSame(CLI.printHelp(),CLI.printHelp());
+        System.out.print(CLI.printHelp());
+
+        Assert.assertEquals("Actual/Expected Help output doesn't match.", expect, outContent.toString());
+
+        assertEquals("Actual/Expected Help strings don't match.", expect, CLI.printHelp());
     }
 
 }
